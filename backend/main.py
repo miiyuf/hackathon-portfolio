411d2ccc8d64e55a3499f47c2e5e5a0284f28428
azu <yubiparu.perc@icloud.com>
2025-08-04 18:18:03 +0900

    follow the new version of flask

--- backend/main.py
+++ backend/main.py
@@ -6,33 +6,41 @@ import atexit
 from flask_cors import CORS
 from decimal import Decimal
 
-class DecimalJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Decimal):
-            return float(obj)
-        return super(DecimalJSONEncoder, self).default(obj)
-
 # Blueprint imports
 from app.main.controller.routes import stockget_bp, stockinsert_bp, holdings_bp, transaction_bp, price_bp, portfolio_bp, profitloss_bp
-# from app.main.services.price import price_bp
-# from app.main.services.holding import holdings_bp
-# from app.main.services.transaction import transaction_bp
-# from app.main.services.insert_stocks import stockinsert_bp
-# from app.main.services.get_stock import stockget_bp
-# from app.main.services.portfolio import portfolio_bp
-# from app.main.services.profit_loss import profitloss_bp
-
-# 
-# from app.main.service.calc_portfolio import update_current_prices
 from app.main.service.repo_service import update_current_prices
 
 # Load .env file
 load_dotenv()
 
 app = Flask(__name__)
-app.json_encoder = DecimalJSONEncoder
-CORS(app, origins="http://localhost:5173")
 
+try:
+    # Flask 2.3+
+    @app.json.provider_class
+    class CustomJSONProvider(app.json.provider):
+        def dumps(self, obj, **kwargs):
+            # Decimal -> Float
+            def _preprocess_decimal(o):
+                if isinstance(o, Decimal):
+                    return float(o)
+                elif isinstance(o, dict):
+                    return {k: _preprocess_decimal(v) for k, v in o.items()}
+                elif isinstance(o, (list, tuple)):
+                    return [_preprocess_decimal(x) for x in o]
+                return o
+                
+            return super().dumps(_preprocess_decimal(obj), **kwargs)
+except (AttributeError, TypeError):
+    class DecimalJSONEncoder(JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Decimal):
+                return float(obj)
+            return super(DecimalJSONEncoder, self).default(obj)
+    
+    app.json_encoder = DecimalJSONEncoder
+
+CORS(app, origins="http://localhost:5173")
 
 # Register blueprints for stock and trade routes
 app.register_blueprint(stockinsert_bp)
@@ -50,8 +58,6 @@ scheduler.start()
 
 # Ensure the scheduler shuts down when the app exits
 atexit.register(lambda: scheduler.shutdown())
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
