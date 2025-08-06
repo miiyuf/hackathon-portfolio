d40b6fd46f4177f142d962a4542b112f6773ece9
miiyuf <miiyu.coding@gmail.com>
2025-08-06 17:03:35 +0900

    add icons

--- frontend/src/components/TotalPortfolioInfo.tsx
+++ frontend/src/components/TotalPortfolioInfo.tsx
@@ -6,6 +6,8 @@ import {
     usePortfolioInfoContext,
 } from '../contexts/PortfolioInfoContext'
 import { updatePortfolioBalance } from '../contexts/PortfolioInfoContext'
+import TrendingUpIcon from '@mui/icons-material/TrendingUp'
+import TrendingDownIcon from '@mui/icons-material/TrendingDown'
 
 // total portfolio balance = current price * owned stock
 // total portfolio investment = cost price * owned stock
@@ -21,18 +23,35 @@ function TotalPortfolioInfo() {
 
     return (
         <div style={{ textAlign: 'left', paddingTop: 10, paddingBottom: 20 }}>
-            <Typography
-                variant="h4"
+            <div
                 style={{
-                    color:
-                        Number(portfolioInfoState.portfolioBalance) > 0
-                            ? 'green'
-                            : 'red',
+                    display: 'flex',
+                    alignItems: 'center',
                 }}
             >
-                Total Portfolio Balance: $
-                {Number(portfolioInfoState.portfolioBalance).toFixed(2)}
-            </Typography>
+                <Typography variant="h4">
+                    Total Portfolio Balance:{' '}
+                    <span
+                        style={{
+                            color:
+                                Number(portfolioInfoState.portfolioBalance) > 0
+                                    ? '#55cb38ff'
+                                    : 'red',
+                        }}
+                    >
+                        $
+                        {Number(portfolioInfoState.portfolioBalance).toFixed(2)}
+                    </span>
+                </Typography>
+                <div style={{ marginLeft: 10 }}>
+                    {Number(portfolioInfoState.portfolioBalance) > 0 ? (
+                        <TrendingUpIcon style={{ color: '#55cb38ff' }} />
+                    ) : (
+                        <TrendingDownIcon style={{ color: 'red' }} />
+                    )}
+                </div>
+            </div>
+
             <Typography variant="h5" color="gray" mt={1}>
                 Total Portfolio Investment: $
                 {Number(portfolioInfoState.portfolioInvestment).toFixed(2)}
